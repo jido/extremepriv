13b399eb276cf725a034f61e27abec14dbd0e11f
jido <denisgithub@bredelet.com>
2024-01-21 10:43:01 +0000

    Add support for partials in mistigri templates

--- privateserver/src/mistigri-engine.ts
+++ privateserver/src/mistigri-engine.ts
@@ -1,10 +1,16 @@
 import { prrcess } from 'mistigri';
 import { promises as fs } from 'fs';
+import { join, extname, dirname, isAbsolute } from 'path';
 import * as escapeHtml from 'escape-html';
 
 async function render(filePath: string, data: any) {
     const template = await fs.readFile(filePath, 'utf8');
-    const page = await prrcess(template, data, {escapeFunction: escapeHtml});
+    const readPartial = function(name: string) {
+        const path = isAbsolute(name) ? name : join(dirname(filePath), name);
+        const ext = extname(name) ? '' : extname(filePath);
+        return fs.readFile(path + ext, 'utf8')
+    }
+    const page = await prrcess(template, data, { escapeFunction: escapeHtml, reader: readPartial });
     return page;
 }
 
