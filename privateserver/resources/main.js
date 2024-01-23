6d7094a1c9c5c9168eca9adf8595cb9548f41305
jido <denisgithub@bredelet.com>
2024-01-23 22:33:03 +0000

    Add key upload
    To test:
    
    1. Load the page
    2. Download the secret key
    3. Click on "Continue" once -> page update loads
    4. Reset user id
    5. Click on "Continue" -> fails because the key is missing/wrong
    6. If key with id 100 is in IndexedDB key store, remove it (e.g. clear site data)
    7. Upload the secret key from step 2
    8. Click on "Continue" -> loads using the decrypted private information
    
    That shows that the key can decrypt the private info after download and upload.

--- privateserver/resources/main.js
+++ privateserver/resources/main.js
@@ -1,13 +1,17 @@
+// Helper functions
+
 const keyAlgorithm = {
     name: "AES-GCM",
     length: 256
 };
 
+const keyUsages = ["encrypt", "decrypt"];
+
 // Generate secret key (keep it safe!)
 const generateKey = window.crypto.subtle.generateKey(
     keyAlgorithm,
     true,
-    ["encrypt", "decrypt"]
+    keyUsages
 );
 
 function keyAsText(getKey) {
@@ -17,6 +21,20 @@ function keyAsText(getKey) {
         const bytes = new Uint8Array(rawKey);
         const data = String.fromCharCode.apply(null, bytes);
         return window.btoa(data);
+    });
+}
+
+function keyFromText(getText) {
+    return getText.then(text => {
+        const binary = window.atob(text);
+        const bytes = Uint8Array.from(binary, b => b.codePointAt(0));
+        return window.crypto.subtle.importKey(
+            "raw",
+            bytes,
+            keyAlgorithm,
+            true,
+            keyUsages
+        );
     });
 }
 
@@ -46,18 +64,6 @@ function decryptMessage(input, key) {
         decoder.decode(decrypted)
     );
 }
-
-var secure_pii = null;
-
-function savePII(pii, key) {
-    const message = JSON.stringify(pii);
-    return encryptMessage(message, key).then(encrypted => {
-        secure_pii = encrypted;
-        return encrypted;
-    });
-}
-
-var user_id = null;
 
 const dbName = "SiteSecrets";
 const dbVer = 2;
@@ -94,52 +100,15 @@ function useDB(openRequest, forWrite) {
     });
 }
 
-function createAccount(pii) {
-    const sendAddRequest = function(secure_pii) {
-        // TODO: send data to server
-        return Math.floor(Math.random() * 1000000) + 1;
-    }
-
-    const openDB = useDB(window.indexedDB.open(dbName, dbVer), true);
-
-    const storeSecretKey = function(id, key) {
-        return openDB.then(db =>
-            new Promise((resolve, reject) => {
-                const transact = db.transaction(keyStore, "readwrite");
-                transact.oncomplete = resolve;
-
-                const addRequest = transact
-                    .objectStore(keyStore)
-                    .add({ id: id, key: key });
-
-                addRequest.onerror = () => {
-                    reject("Saving the new key failed: " + addRequest.error);
-                };
-            })
-        ).catch(message => {
-            throw new Error(message);
-        });
-    }
-
-    return generateKey.then(key =>
-        savePII(pii, key).then(secure_pii =>
-            sendAddRequest(secure_pii)
-        ).then(id => {
-            user_id = id;
-            return storeSecretKey(id, key);
-        }
-    ));
-}
-
-function getSecretKey(db) {
-    return new Promise(resolve => {
+function getSecretKey(db, id) {
+    return new Promise((resolve, reject) => {
         const getRequest = db
             .transaction(keyStore)
             .objectStore(keyStore)
-            .get(user_id);
+            .get(id);
 
         getRequest.onerror = () => {
-            throw new Error("Key not found: " + getRequest.error);
+            reject("Key not found: " + getRequest.error);
         }
 
         getRequest.onsuccess = (event) => {
@@ -148,43 +117,96 @@ function getSecretKey(db) {
     });
 }
 
+function storeSecretKey(db, id, key) {
+    return new Promise((resolve, reject) => {
+        const transact = db.transaction(keyStore, "readwrite");
+        transact.oncomplete = resolve;
+
+        const addRequest = transact
+            .objectStore(keyStore)
+            .add({ id: id, key: key });
+
+        addRequest.onerror = () => {
+            reject("Saving the key failed: " + addRequest.error);
+        }
+    });
+}
+
+function getTemplate(name) {
+    const template = name.replaceAll(".", "").replaceAll(":", "");    // Sanitise name
+
+    return fetch(template + ".mi").then(res => {
+        if (!res.ok) {
+            throw new Error(`Status: HTTP ${res.status} - Unable to load update.`);
+        }
+        return res.text();
+    });
+}
+
+function getHtml(template, pii) {
+    return mistigri.prrcess(
+        template,
+        {
+            ...pii,
+            id: user_id,
+            // The following is for demo purposes...
+            today: new Date(),
+            getDate: o => new Date(o.from),
+            isSameMonth: o => (o.a.getMonth() === o.b.getMonth())
+        },
+        {methodCall: true}
+    );
+}
+
+
+// Page data
+
+var user_id = null;
+var secure_pii = null;
+
+function savePII(pii, key) {
+    const message = JSON.stringify(pii);
+    return encryptMessage(message, key).then(encrypted => {
+        secure_pii = encrypted;
+        return encrypted;
+    });
+}
+
+
+// Functions to use in the page
+
+function createAccount(pii) {
+    const sendAddRequest = function(secure_pii) {
+        // TODO: send data to server
+        return Math.floor(Math.random() * 1000000) + 1;
+    }
+
+    const openDB = useDB(window.indexedDB.open(dbName, dbVer), true);
+
+    return generateKey.then(key =>
+        savePII(pii, key).then(secure_pii =>
+            sendAddRequest(secure_pii)
+        ).then(id => {
+            user_id = id;
+            return openDB.then(db =>
+                storeSecretKey(db, id, key)
+            );
+        }
+    ));
+}
+
 function loadPageUpdate(name, target) {
     //console.log("loadPageUpdate called on: " + target.innerHTML);
     const getPII = useDB(window.indexedDB.open(dbName, dbVer)).then(db =>
-        getSecretKey(db)
+        getSecretKey(db, user_id)
     ).then(key =>
         decryptMessage(secure_pii, key)
     );
 
-    const getHtml = function(template) {
-        return getPII.then(pii =>
-            mistigri.prrcess(
-                template,
-                {
-                    ...JSON.parse(pii),
-                    id: user_id,
-                    today: new Date(),
-                    getDate: o => new Date(o.from),
-                    isSameMonth: o => (o.a.getMonth() === o.b.getMonth())
-                },
-                {methodCall: true}
-            )
-        );
-    }
-
-    const getTemplate = function(name) {
-        const template = name.replaceAll(".", "").replaceAll(":", "");    // Sanitise name
-
-        return fetch(template + ".mi").then(res => {
-            if (!res.ok) {
-                throw new Error(`Status: HTTP ${res.status} - Unable to load update.`);
-            }
-            return res.text();
-        });
-    }
-
     return getTemplate(name).then(template =>
-        getHtml(template)
+        getPII.then(pii =>
+            getHtml(template, JSON.parse(pii))
+        )
     ).then(html => {
         target.outerHTML = html;
     });
@@ -194,7 +216,7 @@ function downloadSecretKey() {
     const fileOptions = { suggestedName: `${user_id}.privatekey` };
 
     return useDB(window.indexedDB.open(dbName, dbVer)).then(db =>
-        keyAsText(getSecretKey(db))
+        keyAsText(getSecretKey(db, user_id))
     ).then(data => {
         if (!!window.showSaveFilePicker) {
             return window.showSaveFilePicker(fileOptions).then(handle =>
@@ -205,9 +227,9 @@ function downloadSecretKey() {
             ));
         }
         else {
-            const blob = new Blob([data], { type: 'application/octet-stream' });
+            const blob = new Blob([data], { type: "application/octet-stream" });
             const objectUrl = URL.createObjectURL(blob);
-            const link = document.createElement('a');
+            const link = document.createElement("a");
             link.download = fileOptions.suggestedName;
             link.href = objectUrl;
             link.click();
@@ -216,5 +238,57 @@ function downloadSecretKey() {
         }
     });
 }
+
+function isUserWithKey(id) {
+    return useDB(window.indexedDB.open(dbName, dbVer)).then(db =>
+        getSecretKey(db, id)
+    ).then(key =>
+        true
+    ).catch(e =>
+        false
+    );
+}
+
+function uploadSecretKey(id) {
+    const openDB = useDB(window.indexedDB.open(dbName, dbVer), true);
+
+    const fileOptions = {
+        types: [{
+            description: "Secret Keys",
+            accept: {
+                "application/*": [".privatekey"]
+            }
+        }]
+    };
+    
+    if (!!window.showOpenFilePicker) {
+        return window.showOpenFilePicker(fileOptions).then(handles =>
+            handles[0].getFile()
+        ).then(file =>
+            keyFromText(file.text())
+        ).then(key =>
+            openDB.then(db =>
+                storeSecretKey(db, id, key)
+            )
+        );
+    }
+    else {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.onchange = event => { 
+            const file = event.target.files[0]; 
+            return keyFromText(file.text()).then(key =>
+                openDB.then(db =>
+                    storeSecretKey(db, id, key)
+                )
+            );
+        }
+        input.click();
+        input.remove();
+    }
+}
+
+
+// Sample data
 
 createAccount({ firstName: "jido", dob: new Date("1999-01-01") });
